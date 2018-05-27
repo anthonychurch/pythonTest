42c182bf1974bfcf61cd8ca28575f7d9641bb1c2
anthonychurch <aw_church@yahoo.com.au>
2018-05-27 14:02:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ In this case the @app.route('/') decorator is calling the app.route('/')(index)(
 """
 @app.route('/')
 def home():
-  user = e.username
+  user = e.user
   return 'stuff'
 
 if __name__ == '__main__user':
