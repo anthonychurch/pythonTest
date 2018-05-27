e126411b26dee50190228a60687204d9c349aaac
anthonychurch <aw_church@yahoo.com.au>
2018-05-27 13:55:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-"""
-Import a class from a subfolder and instantiating the class.
-"""
-from people import employee.
-e = employee.Employee()
+#"""
+#Import a class from a subfolder and instantiating the class.
+#"""
+#from people import employee.
+#e = employee.Employee()
 
-"""
-@app.route('/') is a decorator.
-In this case the @app.route('/') decorator is calling the app.route('/')(index)().
-"""
+#"""
+#@app.route('/') is a decorator.
+#In this case the @app.route('/') decorator is calling the app.route('/')(index)().
+#"""
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
