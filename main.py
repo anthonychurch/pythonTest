bd08075a5123c65451015af94631fca79e6f2bfe
anthonychurch <aw_church@yahoo.com.au>
2018-05-27 12:58:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 import test
+t = test.test()
 
 @app.route('/')
 def hello_world():
-  t = test.test()
+  
   return 'Hello, World!'
 
 if __name__ == '__main__':
