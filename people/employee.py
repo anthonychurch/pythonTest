91002a09c3f05a3d8cb0db1567e86817fcc9ab28
anthonychurch <aw_church@yahoo.com.au>
2018-05-27 13:22:11 -0700

    Add files via upload

---
+++ people/employee.py
@@ -0,0 +1,12 @@
+
+class Employee:
+
+	def __init__(self):
+		self.user = 'username'
+		self.password = 'password'
+	
+	def getUser(self):
+		return self.user
+	
+	def getPassword(self):
+		return self.password
